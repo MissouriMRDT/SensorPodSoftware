db72d55481adf065aed4c4ddaa850a7adbe822f9
andrewvh4 <31864022+andrewvh4@users.noreply.github.com>
2018-04-01 15:25:54 -0500

    Initial blank project template

---
+++ SensorPodSoftware.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  #include <RoveBoard.h>
+  #include <RoveComm.h>
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
